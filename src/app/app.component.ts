3cac01f681674154c85ec4a9b9058600a4503306
Mohamed Hamza <mosh2151984@yahoo.com>
2017-09-05 22:09:14 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   options: DatePickerOptions;
 
   constructor() {
-    this.options = new DatePickerOptions();
+    this.options = new DatePickerOptions({
+        minDate: new Date()
+    
+    });
   }
 }
